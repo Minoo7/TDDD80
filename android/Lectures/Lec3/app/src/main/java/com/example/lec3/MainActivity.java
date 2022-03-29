99a70fa3f8e8e1a804cd55c5fb8446f2339c60be
minoo7 <accounts@minoo.pro>
2022-03-29 16:42:55 +0200

    fortsättning av a3 - skickar filer till pc

--- android/Lectures/Lec3/app/src/main/java/com/example/lec3/MainActivity.java
+++ android/Lectures/Lec3/app/src/main/java/com/example/lec3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lec3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,12 +12,22 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((Button) findViewById(R.id.button)).setOnClickListener((View) -> doNetworkCall());
+        textView = findViewById(R.id.output);
+    }
 
+    private void doNetworkCall() {
+        Thread thread = new Thread(() -> {
+            String data = getData("https://www.ida.liu.se/~andla63/json/test.json");
+            textView.post(() -> textView.setText(data));
+        });
+        thread.start();
     }
 
     private String getData(String theUrl) {
