fa877a9a6841da201b62456c4bfefdd338a512ff
minoo7 <accounts@minoo.pro>
2022-04-23 13:24:55 +0200

    fixed only needed requirements

--- MyProject/server/main.py
+++ MyProject/server/main.py
@@ -1,15 +1,3 @@
-import random
-import string
-
-import groups
-from models import *
-# from server import *
-from flask_wtf import FlaskForm
-from password_validator import PasswordValidator
-from email_validator import validate_email, EmailNotValidError
-
-from validate import *
-from models import *
 
 """def add_message(new_message, sender):
     message = Message(message=new_message)
--- MyProject/test2.py
+++ MyProject/test2.py
@@ -1,11 +1,9 @@
-from enum import Enum
-
 from server import groups, validate
 from server.models import Customer, session
 
 customer = Customer(username='VIN123', password='pass123', first_name='Vincent', last_name='Garbrant',
-                   email='email@gmail.com', gender=groups.Genders.Man, customer_number="VIKD123",
-                   phone_number="070123123", business_type=groups.BusinessTypes.Restaurant, organization_number=101010)
+					email='email@gmail.com', gender=groups.Genders.Man, customer_number="VIKD123",
+					phone_number="070123123", business_type=groups.BusinessTypes.Restaurant, organization_number=101010)
 session.add(customer)
 session.commit()
 
