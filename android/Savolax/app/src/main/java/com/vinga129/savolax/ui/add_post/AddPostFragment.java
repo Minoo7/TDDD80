348522fbadfbdf6108bf94fb66dd27934597e201
minoo7 <accounts@minoo.pro>
2022-06-15 21:39:16 +0200

    added files from laptop

--- android/Savolax/app/src/main/java/com/vinga129/savolax/ui/add_post/AddPostFragment.java
+++ android/Savolax/app/src/main/java/com/vinga129/savolax/ui/add_post/AddPostFragment.java
@@ -1,39 +1,78 @@
 package com.vinga129.savolax.ui.add_post;
 
-import androidx.lifecycle.ViewModelProvider;
+import static com.vinga129.savolax.HelperUtil.properFormValue;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts.GetContent;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.JsonObject;
+import com.vinga129.savolax.data.UserView;
 import com.vinga129.savolax.databinding.FragmentAddPostBinding;
+import com.vinga129.savolax.ui.FormFragment;
+import com.vinga129.savolax.ui.retrofit.rest_objects.Post;
 
-public class AddPostFragment extends Fragment {
+public class AddPostFragment extends FormFragment<Post, FragmentAddPostBinding> {
 
-    private FragmentAddPostBinding binding;
+    private AddPostViewModel addPostViewModel;
+    public static final int PICK_FILE = 99;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        AddPostViewModel addPostViewModel = new ViewModelProvider(this).get(AddPostViewModel.class);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addPostViewModel = new ViewModelProvider(this).get(AddPostViewModel.class);
+        binding.setViewmodel(addPostViewModel);
 
-        binding = FragmentAddPostBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        /*binding.buttonAddPhoto.setOnClickListener((View) -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(intent, PICK_FILE);
+        });*/
 
-        final TextView textView = binding.textPost;
-        addPostViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        ActivityResultLauncher<String> mGetContent = registerForActivityResult(new GetContent(),
+                uri -> {
+                    // Handle the returned Uri
+                    //Toast.makeText(context.getApplicationContext(), "ddddd", Toast.LENGTH_LONG).show();
+                    Snackbar.make(binding.fieldTitle, "resulted..", Snackbar.LENGTH_LONG).show();
+                });
+
+        binding.buttonAddPhoto.setOnClickListener((View) -> {
+            mGetContent.launch("image/*");
+        });
+    }
+
+    @Override
+    protected void initFragmentImpl() {
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public Post getFormData() {
+        JsonObject formData = new JsonObject();
+        formData.add("title", properFormValue(binding.fieldTitle));
+        return null;
+    }
+
+    @Override
+    public void updateUi(UserView model) {
+
+    }
+
+    @Override
+    public void showFail() {
+
     }
 }
--- android/Savolax/app/src/main/java/com/vinga129/savolax/ui/add_post/AddPostViewModel.java
+++ android/Savolax/app/src/main/java/com/vinga129/savolax/ui/add_post/AddPostViewModel.java
@@ -4,16 +4,14 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.vinga129.savolax.ui.retrofit.rest_objects.groups;
+
 public class AddPostViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private MutableLiveData<String[]> post_types = new MutableLiveData<>(groups.enumToStrings(groups.PostTypes.values(),
+            groups.PostTypes::name));
 
-    public AddPostViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String[]> getPostTypes() {
+        return post_types;
     }
 }
